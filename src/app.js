63184c51fe11a6eefdad5cda3758fedec9146c61
Diego Souza <dee-souza@hotmail.com>
2020-04-08 12:40:03 -0300

    :fire: Remove code desnecessary

--- src/app.js
+++ src/app.js
@@ -82,7 +82,6 @@ app.delete("/repositories/:id", (request, response) => {
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
-  const { likes } = request.body;
 
   const repositoryIndex = repositories.findIndex(repo => repo.id === id);
 
